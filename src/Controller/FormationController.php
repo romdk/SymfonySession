b48003366cc3547d2ee388c51f9624c037bf4fb0
romdk <drack.roman67@gmail.com>
2022-12-29 18:44:32 +0100

    recuperer data bdd

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Formation;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FormationController extends AbstractController
 {
     #[Route('/formation', name: 'app_formation')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        // récuperer les formations de la bdd
+        $formations = $doctrine->getRepository(Formation::class)->findAll();
         return $this->render('formation/index.html.twig', [
-            'controller_name' => 'FormationController',
+            'formations' => $formations
         ]);
     }
+
+    #[Route('/formation/{id}', name: 'detail_formation')]
+    public function detail(Formation $formation): Response
+    {   
+        // $sessions = $doctrine->getRepository(Session::class)->findByFormation();
+        return $this->render('formation/detail.html.twig', [
+            'formation' => $formation,
+            // 'sessions' => $sessions
+        ]);
+    }
+
 }
--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Session;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SessionController extends AbstractController
 {
     #[Route('/session', name: 'app_session')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        // récuperer les sessions de la bdd
+        $sessions = $doctrine->getRepository(Session::class)->findAll();
+        $today = date('d/m/Y');
         return $this->render('session/index.html.twig', [
-            'controller_name' => 'SessionController',
+            'sessions' => $sessions,
+            'today' => $today,
         ]);
     }
 }
