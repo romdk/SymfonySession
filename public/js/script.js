abeeb5bfcbffa81ac88cf8ade991f137995a5f2e
romdk <drack.roman67@gmail.com>
2023-01-02 18:04:42 +0100

    script choix session

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+// ------------------------------------- Nav Menu -----------------------------
 const btnFormations = document.getElementById('btnFormations')
 const btnSessions = document.getElementById('btnSessions')
 const btnStagiaires = document.getElementById('btnStagiaires')
@@ -29,4 +30,39 @@ btnProfil.addEventListener('click', () => {
     btnSessions.classList.remove('btnActive')
     btnStagiaires.classList.remove('btnActive')
     btnProfil.classList.add('btnActive')
+})
+
+// --------------------------------Tableaux Sessions --------------------------------
+const currentBtn = document.getElementById('currentBtn')
+const upcomingBtn = document.getElementById('upcomingBtn')
+const pastBtn = document.getElementById('pastBtn')
+const currentSession = document.getElementById('currentSession')
+const upcomingSession = document.getElementById('upcomingSession')
+const pastSession = document.getElementById('pastSession')
+
+currentBtn.addEventListener('click', () => {
+    currentBtn.classList.add('btnActive')
+    upcomingBtn.classList.remove('btnActive')
+    pastBtn.classList.remove('btnActive')  
+    currentSession.style.display = 'block'  
+    upcomingSession.style.display = 'none'  
+    pastSession.style.display = 'none'  
+})
+
+upcomingBtn.addEventListener('click', () => {
+    currentBtn.classList.remove('btnActive')
+    upcomingBtn.classList.add('btnActive')
+    pastBtn.classList.remove('btnActive')
+    currentSession.style.display = 'none'  
+    upcomingSession.style.display = 'block'  
+    pastSession.style.display = 'none'   
+})
+
+pastBtn.addEventListener('click', () => {
+    currentBtn.classList.remove('btnActive')
+    upcomingBtn.classList.remove('btnActive')
+    pastBtn.classList.add('btnActive')
+    currentSession.style.display = 'none'  
+    upcomingSession.style.display = 'none'  
+    pastSession.style.display = 'block'     
 })
