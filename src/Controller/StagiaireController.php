c7244b8bb35ba6fd75c3e780d781903f9074e1c4
romdk <drack.roman67@gmail.com>
2023-01-06 14:58:24 +0100

    affichage stagiaires

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Session;
+use App\Entity\Stagiaire;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StagiaireController extends AbstractController
 {
     #[Route('/stagiaire', name: 'app_stagiaire')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        // récuperer les stagiaires de la bdd
+        $stagiaires = $doctrine->getRepository(Stagiaire::class)->findAll();
         return $this->render('stagiaire/index.html.twig', [
-            'controller_name' => 'StagiaireController',
+            'stagiaires' => $stagiaires
         ]);
     }
 }
