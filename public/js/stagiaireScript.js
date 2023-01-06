084cdaaa912739bde7d703f17091e269a9d3447e
romdk <drack.roman67@gmail.com>
2023-01-06 17:06:08 +0100

    script aff formulaire d'ajout stagiaire

--- public/js/stagiaireScript.js
+++ public/js/stagiaireScript.js
@@ -22,3 +22,17 @@ function affStagiaires() {
         }
     }
 }
+
+
+
+const btnAjout = document.getElementById('btnAjout');
+const overlay = document.getElementById('overlay');
+const btnFermer = document.getElementById('btnFermer');
+
+btnAjout.addEventListener('click', () => {
+ overlay.style.display = 'flex';
+});
+
+btnFermer.addEventListener('click', () => {
+    overlay.style.display = 'none';
+});
