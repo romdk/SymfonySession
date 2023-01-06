dc36ec21d81eeb9d5f47ca2e856fb226ef422019
romdk <drack.roman67@gmail.com>
2023-01-06 13:31:29 +0100

    création formulaires

---
+++ src/Form/SessionType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Session;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SessionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomSession')
+            ->add('dateDebut')
+            ->add('dateFin')
+            ->add('nbPlacesTotal')
+            ->add('referent')
+            ->add('formation')
+            ->add('stagiaires')
+            ->add('programme')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Session::class,
+        ]);
+    }
+}
---
+++ src/Form/StagiaireType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Stagiaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StagiaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('sexe')
+            ->add('dateNaissance')
+            ->add('ville')
+            ->add('email')
+            ->add('telephone')
+            ->add('sessions')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Stagiaire::class,
+        ]);
+    }
+}
