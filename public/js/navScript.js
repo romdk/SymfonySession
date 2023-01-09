9b7f22ad1b1df91eb08db8de423c89da32ace6ec
romdk <drack.roman67@gmail.com>
2023-01-09 19:40:30 +0100

    modif scripts

--- public/js/navScript.js
+++ public/js/navScript.js
@@ -31,38 +31,3 @@ btnProfil.addEventListener('click', () => {
     btnStagiaires.classList.remove('btnActive')
     btnProfil.classList.add('btnActive')
 })
-
-// --------------------------------Tableaux Sessions --------------------------------
-const currentBtn = document.getElementById('currentBtn')
-const upcomingBtn = document.getElementById('upcomingBtn')
-const pastBtn = document.getElementById('pastBtn')
-const currentSessions = document.getElementById('currentSessions')
-const upcomingSessions = document.getElementById('upcomingSessions')
-const pastSessions = document.getElementById('pastSessions')
-
-currentBtn.addEventListener('click', () => {
-    currentBtn.classList.add('btnActive')
-    upcomingBtn.classList.remove('btnActive')
-    pastBtn.classList.remove('btnActive')  
-    currentSessions.style.display = 'block'  
-    upcomingSessions.style.display = 'none'  
-    pastSessions.style.display = 'none'  
-})
-
-upcomingBtn.addEventListener('click', () => {
-    currentBtn.classList.remove('btnActive')
-    upcomingBtn.classList.add('btnActive')
-    pastBtn.classList.remove('btnActive')
-    currentSessions.style.display = 'none'  
-    upcomingSessions.style.display = 'block'  
-    pastSessions.style.display = 'none'   
-})
-
-pastBtn.addEventListener('click', () => {
-    currentBtn.classList.remove('btnActive')
-    upcomingBtn.classList.remove('btnActive')
-    pastBtn.classList.add('btnActive')
-    currentSessions.style.display = 'none'  
-    upcomingSessions.style.display = 'none'  
-    pastSessions.style.display = 'block'     
-})
