82cd02234aa144a863a807a5ca8b7378cb5e665e
romdk <drack.roman67@gmail.com>
2023-01-13 10:07:20 +0100

    formulaire ajout formation

--- src/Form/FormationType.php
+++ src/Form/FormationType.php
@@ -6,13 +6,16 @@ use App\Entity\Formation;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class FormationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nomFormation')
+            ->add('nomFormation', TextType::class)
+            ->add('ajouter', SubmitType::class)
         ;
     }
 
